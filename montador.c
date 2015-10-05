874e460db3bd1d2b7ddb7ea70962f98ec62cfdc2
Gabriel Caê Bounous <gccbounous@gmail.com>
2015-10-05 20:41:08 -0300

    nome dos arquivos de entrada e saida em relaçao aos parametros funcionando

--- montador.c
+++ montador.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "retornaTokens.h"
 
 int main(int argc, char *argv[]) {
-
-	char *nomeArqOut;
-
+	char *nomeArqOut = (char *) malloc(100);
+	//tratamento caso tenha o nome do arquivo de saida ou nao
 	if( argc < 2)
 	{
 		printf("Lançar o programa no formato:\n\t./montador arquivo_de_entrada [arquivo_de_saida]\n");
 	}
 	else if( argc == 2)
 	{
-		retornaTokens(argv[1], NULL);
+		strcpy(nomeArqOut, limpaNomeArquivo(argv[1]));
+		retornaTokens(argv[1], nomeArqOut);
 	}
 	else
 	{
-		nomeArqOut = argv[2];
+		printf("1- %s\n", argv[2]);
+		strcpy(nomeArqOut, limpaNomeArquivo(argv[2]));
+		printf("1- %s\n", nomeArqOut);
 		retornaTokens(argv[1], nomeArqOut);
 	}
 
+	free(nomeArqOut);
     return 0;
 }
--- retornaTokens.c
+++ retornaTokens.c
@@ -4,55 +4,48 @@
 
 #include "retornaTokens.h"
 
+/*
+* Metodo que recebe o nome do arquivo de entrada e de saida (opcional) e limpa o codigo dos comentarios e retira dele os tokens necessarios para o tratamento. 
+* Os tokens sao registrados em um arquivo .out
+*/
 void retornaTokens(char *nomeArqIn, char *nomeArqOut){
-	char * nomeOut = (char *) malloc(100);
+	FILE *tokens;
+    FILE *arq;
+    char * nomeOut = (char *) malloc(100);
+    char frase[1001];
+    char *token;
 
-	if(nomeArqOut == NULL)
-	{
-		strcpy(nomeOut, limpaNomeArquivo(nomeArqIn));
-		strcat(nomeOut, ".out");
-		printf("%s\n", nomeArqIn);
-		printf("%s\n", nomeOut);
-	}
-	else 
-	{
-		strcpy(nomeOut, limpaNomeArquivo(nomeArqOut));
-		strcat(nomeOut, ".out");
-		printf("%s\n", nomeArqIn);
-		printf("%s\n", nomeOut);
-	}
+    strcpy(nomeOut, nomeArqOut);
+    strcat(nomeOut, ".out");
+    
+    /* abre o arquivo */
+    arq = fopen(nomeArqIn, "r");
+    tokens = fopen(nomeOut, "w+");
 
-    // FILE *tokens;
-    // FILE *arq;
-    // char frase[1001];
-    // char *token;
+    while(fgets(frase, 1000, arq)){
+        token = strtok(frase," \t\n");
+        while(token != NULL && *token != '#' && *token != ';'){
+            /* escreve o token */
+            fprintf(tokens, "%s\n" ,token);
+            token = strtok(NULL," \t\n");
+        }
+    }
 
-    // /* abre o arquivo */
-    // arq = fopen(nomeArqIn, "r");
-    // tokens = fopen("tokens.out", "w+");
-
-    // while(fgets(frase, 1000, arq)){
-    //     token = strtok(frase," \t\n");
-    //     while(token != NULL && *token != '#' && *token != ';'){
-    //         /* escreve o token */
-    //         fprintf(tokens, "%s\n" ,token);
-    //         token = strtok(NULL," \t\n");
-    //     }
-    // }
-
-    // /* fecha os arquivos */
-    // fclose(arq);
-    // fclose(tokens);
+    /* fecha os arquivos */
+    fclose(arq);
+    fclose(tokens);
 
     free(nomeOut);
 }
 
+/*
+* Metodo que recupera o nome de um arquivo e tira a extençao, o retornando "limpo" para ser tratado
+*/
 char* limpaNomeArquivo(char *nomeCompleto){
 	char * nome = (char *) malloc(100);
 	strcpy(nome, nomeCompleto);
+
 	int i = 0;
-
-
 	while(nomeCompleto[i] != '\0')
 	{
 		if (nomeCompleto[i] == '.')
